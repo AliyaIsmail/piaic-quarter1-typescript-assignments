43ecdd554061f08d1c8772c9156c276e445ec281
AliyaIsmail <62792030+AliyaIsmail@users.noreply.github.com>
2023-09-15 03:32:11 -0700

    Create assignment1.ts
    PIAIA quarter-1 TS Assignment # 1

---
+++ assignment1.ts
@@ -0,0 +1,36 @@
+/** assignmeNT 1 TS:
+
+-Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+-Store your name in a variable and print it.
+-Store 10 numbers in different variables.
+-Add all of them and print the SUM.
+-Print the Difference (subtraction).
+-Print the result after multiplying all.
+-Take two numbers and print the division result.
+-Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.**/
+
+var msg:string="Hey Abu Hurairah, I have started learning TYPESCRIPT";
+console.log(msg);
+var userName:string="I am AliyaIsmail";
+console.log(userName);
+var num1:number=1;
+var num2:number=2;
+var num3:number=3;
+var num4:number=4;
+var num5:number=5;
+var num6:number=6;
+var num7:number=7;
+var num8:number=8;
+var num9:number=9;
+var num10:number=10;
+var adding:number=(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10);
+console.log("adding of all numbers:",adding);
+var subtraction:number=(num1-num2-num3-num4-num5-num6-num7-num8-num9-num10);
+console.log("subtraction of all numbers:",subtraction);
+var multiplication:number=(num1*num2*num3*num4*num5*num6*num7*num8*num9*num10);
+console.log("multiplication of all numbers:",multiplication);
+var division:number=(num4/num8);
+console.log("division of two numbers:",division);
+
+//.................completed...............
+
